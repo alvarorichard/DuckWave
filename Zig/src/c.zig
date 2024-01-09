2bd2c505eebbb4128dbcafa1cba0f6142736341f
alvarorichard <alvaro10048@protonmail.com>
2024-01-09 19:59:33 -0300

    add c.zig

---
+++ Zig/src/c.zig
@@ -0,0 +1,11 @@
+pub usingnamespace @cImport({
+  @cInclude("stddef.h");
+    @cInclude("ao/ao.h");
+    @cInclude("stdio.h");
+    @cInclude("stdlib.h");
+    @cInclude("mpg123.h");
+    @cInclude("string.h");
+    @cInclude("termios.h");
+    @cInclude("unistd.h");
+    @cInclude("fcntl.h");
+});
--- Zig/src/playlib.zig
+++ Zig/src/playlib.zig
@@ -1,16 +1,6 @@
 const std = @import("std");
+const c = @import("c.zig");
 
-const c = @cImport({
-    @cInclude("stddef.h");
-    @cInclude("ao/ao.h");
-    @cInclude("stdio.h");
-    @cInclude("stdlib.h");
-    @cInclude("mpg123.h");
-    @cInclude("string.h");
-    @cInclude("termios.h");
-    @cInclude("unistd.h");
-    @cInclude("fcntl.h");
-});
 
 
 pub const PlayMP3 = struct {
