1cc92387f63ab2c0e0c6c4d2a55d88d05f06c5a4
alvarorichard <alvaro10048@protonmail.com>
2024-04-06 01:47:30 -0300

    fix script

--- install.sh
+++ install.sh
@@ -1,70 +1,62 @@
 #!/bin/bash
 
-if clang -v &> /dev/null
-then
+if clang -v &>/dev/null; then
 	COMPILER=$(which clang)
-
-elif gcc -v &> /dev/null
-then
-	COMPILER=$(which clang)
-
+elif gcc -v &>/dev/null; then
+	COMPILER=$(which gcc)
 else
 	echo
-	printf "\033[31m[ERRO]\033[m "
-	echo "Os compiladores 'clang' e 'gcc' não foram encontrados"
+	printf "\033[31m[ERROR]\033[m "
+	echo "Compilers 'clang' and 'gcc' were not found."
 	echo
-
 	exit 1
 fi
 
-## Essa função vai rodar o script `./configure` que a biblioteca (`$1`) deve
-## ter em sua raís, o prefixo dessa configuração será alguma pasata no
-## diretório local ($3).
 function vendorize_local_library {
-	local LIB_NAME="${1}"     #$1: string - Nome da pasta da lib local
-	local LIB_VERSION="${2}"  #$2: string - Versao da lib, arbitrário
-	local VENDOR_DIR="${3}"   #$3: string - Nome da pasta de vendor local
+	local LIB_NAME="${1}"
+	local LIB_VERSION="${2}"
+	local VENDOR_DIR="${3}"
 
-	printf "\033[36m[LOGG]\033[m "
-	echo "Configurando a biblioteca ${LIB_NAME}"
+	printf "\033[36m[LOG]\033[m "
+	echo "Configuring library ${LIB_NAME}"
 
 	local TARGET_PATH="${VENDOR_DIR}/${LIB_NAME}_${LIB_VERSION}"
 
 	mkdir -vp "${TARGET_PATH}"
-	bash -c "cd ${LIB_NAME} && ./configure --prefix=\$(pwd)/../${TARGET_PATH}"
+	bash -c "cd ${LIB_NAME} && ./configure --prefix=\$(pwd)/../${TARGET_PATH} && make && make install"
 }
 
-#TODO: Seria melhor se a pasta `/vendor` tivese a sua própria variável
-#TODO: Essa variável poderia ser alterada com uma opção `--vendor`
-#TODO: O usuário deve ter a opção de skippar essa parte se ele quiser
-vendorize_local_library "libao"  "1.2.2"  "vendor"
-vendorize_local_library "mpg123" "1.26.4" "vendor"
+VENDOR_DIR="vendor"
+
+vendorize_local_library "libao" "1.2.2" "${VENDOR_DIR}"
+vendorize_local_library "mpg123" "1.26.4" "${VENDOR_DIR}"
 
 function compile_statically {
 	echo
-	printf "\033[33m[WARN]\033[m "
-	echo "Esse script não consegue compilar estaticamente ainda!"
+	printf "\033[36m[LOG]\033[m "
+	echo "Compiling the application with static libraries"
 	echo
+
+	eval "${COMPILER} playmp3.c main.c \
+        -I${VENDOR_DIR}/libao_1.2.2/include -I${VENDOR_DIR}/mpg123_1.26.4/include \
+        -L${VENDOR_DIR}/libao_1.2.2/lib -L${VENDOR_DIR}/mpg123_1.26.4/lib \
+        -lao -lmpg123 -lavcodec -lavformat -lswresample -lavutil -lm -lpthread -lz -lssl -lcrypto \
+        -o playmp3"
 }
 
 function compile_shared {
 	echo
-	printf "\033[36m[LOGG]\033[m "
-	echo "Compilando a aplicação com as bibliotecas do sistema"
+	printf "\033[36m[LOG]\033[m "
+	echo "Compiling the application with system libraries"
 	echo
 
-	#TODO: Colocar os arquivos de source em constantes também
-	#TODO: Colocar as flagas de bibliotecas em uma constante
-	#TODO: Deixar o usuário usar flags customizadas se ele quiser
-	#TODO: Por o nome do binário final em uma variável
-	#TODO: O path  do binário pode ser customizado pelo usuário também
 	eval "${COMPILER} playmp3.c main.c \
-		-lao -lavcodec -lavformat -lswresample -lavutil \
-		-o playmp3"
+        -lao -lavcodec -lavformat -lswresample -lavutil \
+        -o playmp3"
 }
 
-#TODO: A função de compilar estaticamente não deve funcionar com `--skip-conf`
-#TODO: Criar a opção `--static` do jeito certo
-[ "${1}" = "--static" ] || [ "${1}" = "-s" ] &&
-	compile_statically ||
+if [ "${1}" = "--static" ] || [ "${1}" = "-s" ]; then
+	compile_statically
+else
 	compile_shared
+fi
