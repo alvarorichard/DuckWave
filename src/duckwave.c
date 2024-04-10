84601b8acf492361277a7bcffdcf353b7bc50685
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-04-10 19:19:48 -0300

    impl: implemented the function body for each function definition

---
+++ src/duckwave.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MINIAUDIO_IMPLEMENTATION
+
+#include "duckwave.h"
+
+void duckwave_init_file_decoder(DuckWaveSoundData* dw_sdata, char* file) {
+  ma_result result;
+
+  result = ma_decoder_init_file(file, NULL, &dw_sdata->decoder);
+
+  if (result != MA_SUCCESS) {
+		printf("\nCould not decode current %s file.\n", file);
+    printf("%s.\n\n", ma_result_description(result));
+
+    exit(1);
+  }
+}
+
+void duckwave_init_device_playback(DuckWaveSoundData* dw_sdata) {
+  ma_decoder* decoder = &dw_sdata->decoder;
+  ma_device_config* device_config = &dw_sdata->device_config;
+
+	device_config->playback.format = decoder->outputFormat;
+	device_config->playback.channels = decoder->outputChannels;
+	device_config->sampleRate = decoder->outputSampleRate;
+	device_config->dataCallback = duckwave_default_playback_callback;
+	device_config->pUserData = decoder;
+}
+
+void duckwave_start_playsound_thread(DuckWaveSoundData* dw_sdata) {
+  ma_result result;
+
+  ma_decoder* decoder = &dw_sdata->decoder;
+	ma_device* device = &dw_sdata->device;
+  ma_device_config* device_config = &dw_sdata->device_config;
+	
+	result = ma_device_init(NULL, device_config, device);
+
+  if (result != MA_SUCCESS) {
+		printf("\nCannot initilize device.\n");
+    printf("%s.\n\n", ma_result_description(result));
+
+		ma_decoder_uninit(decoder);
+
+    exit(2);
+  }
+
+	result = ma_device_start(device);
+
+  if (result != MA_SUCCESS) {
+		printf("\nCannot start device.\n");
+    printf("%s.\n\n", ma_result_description(result));
+
+		ma_device_uninit(device);
+		ma_decoder_uninit(decoder);
+
+    exit(3);
+  }
+}
+
+
