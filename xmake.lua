90706a291afa2ba2724b44072ca8ccafce9b71be
alvarorichard <alvaro10048@protonmail.com>
2024-04-11 18:05:08 -0300

    Add .gitignore entries for generated files and directories and add xmake.lua

---
+++ xmake.lua
@@ -0,0 +1,25 @@
+toolchain("DuckWave_Toolchain")
+    set_kind("standalone")
+
+    set_toolset("cc", "clang")
+    set_toolset("cxx", "clang++", "clang")
+    set_toolset("ld", "clang", "clang++")
+toolchain_end()
+
+target("DuckWave")
+    set_kind("binary")
+
+    -- TODO: In future, is better using globbing instead file to file
+    add_includedirs("include")
+     
+    add_files("src/main.c", "src/duckwave.c")
+
+    set_optimize("fastest")
+
+
+    -- Set C language standard, if needed (e.g., c99, c11)
+    set_languages("c11")
+
+    -- Add any necessary compile flags, if specific flags are needed
+      add_ldflags("-lm")
+
