50c7b06722008f2a750dd5a50c1744c43dd17c1a
alvarorichard <alvaro10048@protonmail.com>
2024-06-30 20:56:13 -0300

    add ncurses to code

--- src/main.c
+++ src/main.c
@@ -1,50 +1,136 @@
-
+#include <math.h>
+#include <ncurses.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "../include/duckwave.h"
 
+// int main(int argc, char *argv[]) {
+//     if (argc < 2) {
+//         printf("You need to specify a song.\n");
+//         return EXIT_FAILURE;
+//     } else if (argc > 2) {
+//         printf("Multiple song files are not allowed right now.\n");
+//         return EXIT_FAILURE;
+//     }
+
+//     char* song_file = argv[1];
+//     DuckWaveSoundData dw_sdata;
+
+//     duckwave_init_file_decoder(&dw_sdata, song_file);
+//     duckwave_init_device_playback(&dw_sdata);
+//     duckwave_start_playsound_thread(&dw_sdata);
+
+//     if (!ma_device_is_started(&dw_sdata.device)) {
+//         printf("Could not start the song.\n");
+//         return EXIT_FAILURE;
+//     }
+
+//     float song_duration = 0.0f, song_cursor = 0.0f; // Inicialização das
+//     variáveis char duration_timestamp[12], cursor_timestamp[12];
+
+//     ma_data_source_get_length_in_seconds(&dw_sdata.decoder, &song_duration);
+//     generate_timestamp(song_duration, duration_timestamp);
+
+//     printf("\x1b[2J\x1b[?25h\x1b[?25l");  // clear the screen & hide cursor
+
+//     while (song_cursor < song_duration) {
+//         ma_data_source_get_cursor_in_seconds(&dw_sdata.decoder,
+//         &song_cursor); generate_timestamp(song_cursor, cursor_timestamp);
+
+//         printf("\x1b[H");  // move the write cursor to the top left, to
+//         overwrite the timestamp
+
+//         printf("%s/%s\n", cursor_timestamp, duration_timestamp);
+//     }
+
+//     ma_device_uninit(&dw_sdata.device);
+//     ma_decoder_uninit(&dw_sdata.decoder);
+
+//     return EXIT_SUCCESS;
+// }
+
+// Função para desenhar gráficos de barras
+void draw_bars(float *data, int count) {
+  clear();
+  int max_height = LINES - 1;
+  for (int i = 0; i < count; i++) {
+    int height = (int)(data[i] * max_height);
+    for (int j = 0; j < height; j++) {
+      mvprintw(max_height - j, i * 2, "|");
+    }
+  }
+  refresh();
+}
+
+// Callback modificado para capturar dados de áudio
+void duckwave_playback_callback_with_visual(ma_device *pDevice, void *pOutput,
+                                            const void *pInput,
+                                            unsigned int frameCount) {
+  ma_decoder *pDecoder = pDevice->pUserData;
+  if (pDecoder == NULL)
+    return;
+
+  ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, NULL);
+
+  // Processar dados de áudio para visualização
+  float *audio_data = (float *)pOutput;
+  float bar_data[80];
+  int samples_per_bar = frameCount / 80;
+  for (int i = 0; i < 80; i++) {
+    float sum = 0;
+    for (int j = 0; j < samples_per_bar; j++) {
+      sum += fabs(audio_data[i * samples_per_bar + j]);
+    }
+    bar_data[i] = sum / samples_per_bar;
+  }
+  draw_bars(bar_data, 80);
+
+  (void)pInput;
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("You need to specify a song.\n");
-        return EXIT_FAILURE;
-    } else if (argc > 2) {
-        printf("Multiple song files are not allowed right now.\n");
-        return EXIT_FAILURE;
-    }
+  if (argc < 2) {
+    printf("Usage: %s <audio_file>\n", argv[0]);
+    return -1;
+  }
 
-    char* song_file = argv[1];
-    DuckWaveSoundData dw_sdata;
+  DuckWaveSoundData dw_sdata;
+  duckwave_init_file_decoder(&dw_sdata, argv[1]);
+  duckwave_init_device_playback(&dw_sdata);
 
-    duckwave_init_file_decoder(&dw_sdata, song_file);
-    duckwave_init_device_playback(&dw_sdata);
-    duckwave_start_playsound_thread(&dw_sdata);
+  // Substituir o callback padrão pelo callback com visualização
+  dw_sdata.device_config.dataCallback = duckwave_playback_callback_with_visual;
 
-    if (!ma_device_is_started(&dw_sdata.device)) {
-        printf("Could not start the song.\n");
-        return EXIT_FAILURE;
-    }
+  initscr();
+  cbreak();
+  noecho();
+  curs_set(FALSE);
 
-    float song_duration = 0.0f, song_cursor = 0.0f; // Inicialização das variáveis
-    char duration_timestamp[12], cursor_timestamp[12];
+  ma_result result =
+      ma_device_init(NULL, &dw_sdata.device_config, &dw_sdata.device);
+  if (result != MA_SUCCESS) {
+    printf("Failed to initialize playback device.\n");
+    return -1;
+  }
 
-    ma_data_source_get_length_in_seconds(&dw_sdata.decoder, &song_duration);
-    generate_timestamp(song_duration, duration_timestamp);
-
-    printf("\x1b[2J\x1b[?25h\x1b[?25l");  // clear the screen & hide cursor
-
-    while (song_cursor < song_duration) {
-        ma_data_source_get_cursor_in_seconds(&dw_sdata.decoder, &song_cursor);
-        generate_timestamp(song_cursor, cursor_timestamp);
-
-        printf("\x1b[H");  // move the write cursor to the top left, to overwrite the timestamp
-
-        printf("%s/%s\n", cursor_timestamp, duration_timestamp);
-    }
-
+  result = ma_device_start(&dw_sdata.device);
+  if (result != MA_SUCCESS) {
+    printf("Failed to start playback device.\n");
     ma_device_uninit(&dw_sdata.device);
-    ma_decoder_uninit(&dw_sdata.decoder);
+    return -1;
+  }
 
-    return EXIT_SUCCESS;
+  while (1) {
+    // Aguardar até que o usuário pressione uma tecla para sair
+    if (getch() != ERR) {
+      break;
+    }
+  }
+
+  ma_device_uninit(&dw_sdata.device);
+  endwin();
+
+  return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -5,32 +5,33 @@ set_allowedmodes("debug", "release")
 set_defaultmode("debug")
 add_rules("mode.debug", "mode.release")
 
-toolchain("dwtc")  -- recommended toolchain
-    set_kind("standalone")
+toolchain("dwtc") -- recommended toolchain
+set_kind("standalone")
 
-    set_toolset("cc", "clang")
-    set_toolset("cxx", "clang++", "clang")
-    set_toolset("ld", "clang", { force = true })  -- ensure mold is used as linker
-    set_toolset("sh", "clang++", "clang")
-    set_toolset("ar", "llvm-ar")
+set_toolset("cc", "clang")
+set_toolset("cxx", "clang++", "clang")
+set_toolset("ld", "clang", { force = true }) -- ensure mold is used as linker
+set_toolset("sh", "clang++", "clang")
+set_toolset("ar", "llvm-ar")
 toolchain_end()
 
 target("duckwave")
-    set_kind("binary")
-    add_includedirs("include")
-    add_files("src/main.c", "src/duckwave.c")
+set_kind("binary")
+add_includedirs("include")
+add_files("src/main.c", "src/duckwave.c")
 
-    if is_mode("release") then
-        set_optimize("fastest")
-        set_symbols("hidden")
+if is_mode("release") then
+	set_optimize("fastest")
+	set_symbols("hidden")
+elseif is_mode("debug") then
+	set_optimize("none")
+	set_symbols("debug")
+end
 
-    elseif is_mode("debug") then
-        set_optimize("none")
-        set_symbols("debug")
-    end
-
-    set_targetdir("bin")
-    set_languages("c17")
-    add_ldflags("-fuse-ld=mold", { force = true })  -- add mold linker flag
-    -- add_links("avformat", "avcodec", "ao", "m")
+set_targetdir("bin")
+set_languages("c17")
+add_ldflags("-fuse-ld=mold", { force = true }) -- add mold linker flag
+-- add_links("avformat", "avcodec", "ao", "m")
+add_links("ncurses", "m")
 target_end()
+
